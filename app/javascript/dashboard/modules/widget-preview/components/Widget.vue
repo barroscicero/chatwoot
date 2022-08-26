e66de0561f35666bf781da597c0b57986ed34be7
Cicero Barros <barrosc@aljazeera.net>
2022-08-25 23:39:54 -0300

    Update readme

--- app/javascript/dashboard/modules/widget-preview/components/Widget.vue
+++ app/javascript/dashboard/modules/widget-preview/components/Widget.vue
@@ -29,7 +29,7 @@
       >
         <img
           v-if="!isWidgetVisible"
-          src="~dashboard/assets/images/bubble-logo.png"
+          src="~dashboard/assets/images/bubble-logo.svg"
           alt=""
         />
         <div>
