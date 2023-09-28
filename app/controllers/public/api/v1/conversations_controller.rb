ca45ab7a43021fbb1a2cb24967fee49c22eb6e2c
Cicero Barros <barrosc@aljazeera.net>
2023-09-28 18:17:35 -0300

    Adicionando API de protocolo

--- app/controllers/public/api/v1/conversations_controller.rb
+++ app/controllers/public/api/v1/conversations_controller.rb
@@ -2,13 +2,13 @@ class Public::Api::V1::ConversationsController < PublicController
 
   def fetch_by_protocolo
     protocolo = params[:protocolo]
-    @conversation = Conversation.where("TRIM(additional_attributes->>'protocolo') = ?", protocolo).first
+    @conversation = Conversation.where("TRIM(additional_attributes->>'protocolo') = ?", protocolo).order(:created_at).reverse_order.first
 
     if @conversation.nil?
       render json: { error: 'Conversation not found for the given protocolo' }, status: :not_found
     else
-      @messages = @conversation.messages.outgoing.order(created_at: :desc).first
-      render json: { conversation: @conversation, messages: @messages }
+      @messages = @conversation.messages.outgoing.where(private: false).order(:created_at).reverse_order.first
+      render json: { conversation: @conversation, messages: @messages  }
     end
   end
 
